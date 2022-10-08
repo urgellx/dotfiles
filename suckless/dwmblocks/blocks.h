ed86b03b4947ee567f6dfb4566966b28e9970916
Urgellx <109094729+urgellx@users.noreply.github.com>
2022-10-08 18:11:06 +0000

    Adding new suckless builds
    New dwm patches and changing the directory name

---
+++ suckless/dwmblocks/blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "echo ^c#ffffff^^b#6b629e^",                                       60,             1},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "/usr/local/bin/music.sh",                                     5,             3},
+	{"", "date '+%b %d (%a) %I:%M%p'",                                    60,             1},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
